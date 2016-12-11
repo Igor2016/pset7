f0284b94f4f6b88e0c7968c0315d7e3d7cca033a
Anant Ashutosh Sharma <anant.sharma.18@gmail.com>
2016-12-11 09:27:55 +0000

    Index.php update

--- pset7/public/index.php
+++ pset7/public/index.php
@@ -4,6 +4,24 @@
     require("../includes/config.php"); 
 
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    $positions = [];
+    $id = $_SESSION["id"];
+    $rows = CS50::query("SELECT * FROM portfolio WHERE user_id = ?", $id);
+    foreach ($rows as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+                "name" => $stock["name"],
+                "price" => $stock["price"],
+                "shares" => $row["shares"],
+                "symbol" => $row["symbol"]
+            ];
+        }
+    }
+    $userRow = CS50::query("SELECT * FROM users WHERE id = ?", $id);
+    $cash = $userRow[0]["cash"];
+    render("portfolio.php", ["positions" => $positions, "cash" => $cash, "title" => "Portfolio"]);
 
 ?>
